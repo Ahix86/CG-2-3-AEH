12518acbb635b681c0ab859e4164e083161b372d
Allison <allison.hix86@gmail.com>
2018-07-17 20:16:10 -0500

    Birthday string

---
+++ CG 2-3 String/CG 2-3 String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CG_2_3_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What month were you born:  ");
+            string month = Console.ReadLine();
+
+            Console.Write("What day were you born:  ");
+            string day = Console.ReadLine();
+
+            Console.Write("What year were you born:  ");
+            string year = Console.ReadLine();
+
+            string birthday = month + " " + day + ", " + year;
+
+            Console.WriteLine("Your birthday is " + birthday + ".");
+            Console.ReadLine();
+
+        }
+    }
+}
